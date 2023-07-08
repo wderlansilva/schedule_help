696522a14ebd931d57330d7f92f8d2d526284098
wderlan silva <wderlansilva@hotmail.com>
2023-07-07 23:35:25 -0300

    Hello world

---
+++ src/main/java/com/schedulehelp/schedule/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.schedulehelp.schedule.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+
+
+    @GetMapping
+    public String getUser() {
+        return "Hello world";
+    }
+
+}
